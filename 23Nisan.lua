679b8429bc4c50cfaf1c8c73673933f41126889b
Ediz Tekok <ediztekok@gmail.com>
2018-08-09 01:24:41 +0300

    Add files via upload

---
+++ 23Nisan.lua
@@ -0,0 +1,329 @@
+--------------------------
+--- 23 Nisan Etkinliği ---
+--------------------------
+-- <DS Y="760" X="2060" />
+map = [[<C><P H="800" L="3600" Ca=""  /><Z><S><S L="1810" o="" X="2695" H="30" Y="787" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="473" o="" X="2780" H="25" Y="718" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="528" o="" X="2780" H="10" Y="732" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="300" o="" X="2150" H="10" Y="615" T="12" P="0,0,0,0.2,50,0,0,0" /><S L="300" o="" X="3450" H="10" Y="620" T="12" P="0,0,0,0.2,-50,0,0,0" /><S L="1090" o="" X="2800" H="10" Y="641" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="1210" o="" X="2800" H="10" Y="572" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="1340" o="" X="2800" H="10" Y="497" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="15" o="" X="3568" H="240" Y="612" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="217" o="" X="3467" H="10" Y="733" T="12" P="0,0,0,0.2,0,0,0,0" /><S L="20" o="" X="3558" H="10" Y="503" T="12" P="0,0,0,0.2,-50,0,0,0" /><S L="20" o="" X="2038" H="10" Y="497" T="12" P="0,0,0,0.2,50,0,0,0" /><S L="15" o="" X="2028" H="246" Y="609" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="222" o="" X="2132" H="10" Y="728" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="734" o="" X="1422" H="30" Y="789" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="1008" o="" X="505" H="30" Y="789" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="386" o="" X="1520" H="30" Y="714" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="380" o="" X="1004" H="30" Y="714" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="500" o="" X="250" H="30" Y="715" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="180" o="" X="101" H="25" Y="652" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="20" o="" X="173" H="42" Y="626" T="12" P="0,0,0.3,0.2,-28,0,0,0" /><S L="20" o="" X="29" H="42" Y="626" T="12" P="0,0,0.3,0.2,28,0,0,0" /><S L="40" o="" X="141" H="13" Y="542" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="40" o="" X="59" H="13" Y="542" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="451" o="" X="550" H="21" Y="595" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="177" o="" X="583" H="10" Y="566" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="147" o="" X="889" H="10" Y="557" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="147" o="" X="1058" H="10" Y="558" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="10" o="" X="1179" H="326" Y="497" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="141" o="" X="1061" H="10" Y="413" T="12" P="0,0,0.3,0.9,0,0,0,0" /><S L="180" o="" X="880" H="10" Y="443" T="12" P="0,0,0.3,0.9,0,0,0,0" /><S L="449" o="" X="551" H="10" Y="405" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="369" o="" X="559" H="10" Y="343" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="59" o="" X="688" H="75" Y="301" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="232" o="" X="116" H="10" Y="424" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="232" o="" X="116" H="10" Y="332" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="150" o="" X="72" H="61" Y="251" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="129" o="" X="65" H="65" Y="193" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="19" o="" X="129" H="62" Y="194" T="12" P="0,0,0.3,0.2,-17,0,0,0" /><S L="188" o="" X="880" H="14" Y="219" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="233" o="" X="907" H="14" Y="168" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="173" o="" X="1130" H="10" Y="193" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="179" o="" X="1087" H="10" Y="286" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S P="0,0,,,,0,0,0" L="41" H="326" Y="497" T="9" m="" X="1179" /><S L="141" o="" X="101" H="10" Y="608" T="12" P="0,0,0.3,0.7,0,0,0,0" /><S L="110" o="" X="55" H="10" Y="327" T="12" P="0,0,0.3,0.2,0,0,0,0" /><S L="10" o="" X="-5" H="800" Y="400" T="12" P="0,0,0.3,0.2,0,0,0,0" /></S><D></D><O /></Z></C>]]
+
+--[[
+names = {"Nehirr", "Mellefromage", "Deneyfaresi", "Transforlays", "Fierying", "Ediz", "Kmlcan", "Teoodora", "Cryquinette", "Aewing", "Jordynl", "Makinit", "Leafileaf", "Momokolee", "Fzqx", "Somnusmortem", "Paperonaruto", "Modozore", "Nihoshi", "Baasbase", "Tweetis"}
+
+for i, n in pairs(names) do
+  system.savePlayerData(n, 0)
+end]]
+
+
+
+
+
+
+translations = {
+  EN = {
+    flags = "Flags",
+    instructions = [[Find the flags, collect and bring them back to the stage!
+Be careful not to fall and be on time to celebrate this special day!]],
+    childrensDay = "Happy Children's Day! May all the little mice bring happiness to future!",
+  },
+  TR = {
+    flags = "Bayraklar",
+    instructions = [[Bayrakları bulun, toplayın ve onları tören alanına geri getirin!
+Düşmemeye dikkat edin ve bu önemli günü kutlamak için zamanında gelin!]],
+    childrensDay = "Ulusal Egemenlik ve Çocuk Bayramınız kutlu olsun!",
+  },
+  RU = {
+    flags = "Флаги",
+    instructions = [[Находи флаги, собирай и приноси на сцену!
+Будь осторожен чтобы не упасть и успеть вовремя к празднованию этого особенного дня!]],
+    childrensDay = "С Днем Детей! Пусть все маленькие мышки принесут счастье будущему!",
+  },
+  BR = {
+    flags = "Bandeiras",
+    instructions = [[Encontre as bandeiras, pegue-as e as traga de volta para a base!
+Tome cuidado para não cair e esteja a tempo de celebrar este dia especial!]],
+    childrensDay = "Feliz dia das crianças! Que todos os pequenos ratinhos tragam alegria para o futuro!",
+  },
+  NL = {
+    flags = "Vlaggen",
+    instructions = [[Zoek en verzamel alle vlaggen, en breng ze terug naar het podium!
+Pas op dat je niet valt en ben op tijd om deze speciale dag te vieren!]],
+    childrensDay = "Fijne Kinderdag! Moge alle kleine muisjes een gelukkige toekomst tegemoet gaan!"
+  },
+  ES = {
+    flags = "Banderas",
+    instructions = [[¡Encuentra las banderas, colecciónalas y tráelas de vuelta al escenario!
+¡Ten cuidado de no caerte y estar a tiempo para celebrar éste día especial!]],
+    childrensDay = "!Feliz día de los Niños! ¡Que todos los pequeños ratones traigan un futuro feliz!"
+  },
+  RO = {
+    flags = "Steaguri",
+    instructions = [[Găsește steagurile, colectează-le și adu-le înapoi pe scenă!
+Ai grijă să nu cazi și să ajungi la timp pentru a celebra această zi specială!]],
+    childrensDay = "La mulți ani de ziua copilului! Fie ca toți șoriceii să aducă bucurie viitorului!",
+  },
+  HU = {
+    flags = "Zászlók",
+    instructions = [[Találd meg a zászlókat, gyűjtsd össze és vidd vissza őket a színpadra!
+Vigyázz, nehogy leess és ünnepeld meg időben ezt a különleges napot!]],
+    childrensDay = "Boldog Gyereknapot! Hozzanak a kisegerek boldogságot a jövőre!",
+  },
+  DE = {
+    flags = "Fahnen",
+    instructions = [[Finde die Flaggen, sammle sie ein und bring sie zurück zur Bühne!
+Sei vorsichtig, fall nicht herunter und sei pünktlich, um diesen speziellen Tag zu feiern!]],
+    childrensDay = "Fijne Kinderdag! Moge alle kleine muisjes een gelukkige toekomst tegemoet gaan!"
+  },
+  VK = {
+    flags = "Flaggene",
+    instructions = [[Finn flaggene, samle og bring dem tilbake til scenen!
+Vær forsiktig så du ikke faller og kom i god tid til å feire denne spesielle dagen!]],
+    childrensDay = "God Barnas Dag! Måtte alle de små mus bringe glede til fremtiden!",
+  },
+  FR = {
+    flags = "Drapeaux",
+    instructions = [[Trouvez les drapeaux, collectez les et rapportez les sur la scène!
+Faites attention à ne pas tomber et d'arriver à temps pour célébrer ce jour spécial!]],
+    childrensDay = "Joyeux jour des enfants! Puissent toutes ces petites souris vous apporter de la joie!",
+  },
+  ID = {
+    flags = "Bendera",
+    instructions = [[Cari bendera-bendera, kumpulkan, dan bawa mereka kembali ke panggung!
+Berhati-hatilah untuk tidak jatuh dan tidak terlambat untuk merayakan hari spesial ini!]],
+    childrensDay = "Selamat Hari Anak! Semoga semua tikus-tikus kecil membawa kebahagiaan di masa depan!",
+  },
+  PL = {
+    flags = "Flagi",
+    instructions = [[Znajdź flagi, zbierz je oraz przynieś z powrotem na scenę!
+Uważaj aby nie spaść i staraj się być na czas aby świętować ten wyjątkowy dzień!]],
+    childrensDay = "Wszystkiego najlepszego z okazji Dnia Dziecka! Życzymy szczęścia wszystkim małym myszkom!",
+  },
+  CN = {
+    flags = "旗帜们",
+    instructions = [[找出旗子, 收集并拿着他们回去舞台!
+小心别掉下来还要准时前往庆祝这个特别的日子!]],
+    childrensDay = "儿童节快乐! 祝所有小老鼠都带着快乐前往未来!"
+  }
+}
+msg = translations.EN -- translations[tfm.get.room.community:upper()] or translations.EN
+fwC = 20
+tm, fwList = 0, {}
+cfIDs = {21, 22, 23, 24}
+fwIDs = {0, 1, 2, 4, 9, 11, 13, 21, 22, 23, 24, 29}
+coords = {829, 508, 1023, 236, 14, 279, 91, 729, 1425, 653, 857, 729, 337, 654, 360, 539, 44, 489, 411, 293, 25, 374, 646, 351, 835, 167, 1061, 144, 64, 116}
+flags = {}
+flagCount = 0
+backgrounds = {}
+bgImg, fgImg = "2vJE3oQ.jpg", "6WeBwlL.png"
+pFL, playerFlagCount, playerFlag = {}, {}, {}
+flagImages = {"uO4mL4s.png", "3Hqa7jO.png", "3gixa6h.png", "X1lrQRA.png", "8dE9sMq.png", "ntrBR4b.png", "KhR4nyv.png", "4OlPqgj.png", "EKzIddT.png", "wZp8tJk.png", "7r4eUkH.png", "zUgxQ2E.png", "2dpSPE6.png", "KSHbIXw.png", "VDLAYln.png"}
+
+function load(n)
+  table.insert(backgrounds, tfm.exec.addImage(bgImg, '?1', 0, 0, n))
+  table.insert(backgrounds, tfm.exec.addImage(fgImg, '!999', 0, 0, n))
+end
+
+function createRP(t, x, y, vx, vy)
+  tfm.exec.displayParticle(t[math.random(#t)], x, y, vx, vy, 0, 0.08)
+end
+
+function throwCF()
+  for i=1,50 do
+    createRP(cfIDs, 2465, 725, math.random(-3,3), -math.random(1,3))
+    createRP(cfIDs, 3080, 725, math.random(-3,3), -math.random(1,3))
+  end
+end
+
+function explodeFW(x, y)
+  local a, r, pa = (math.pi * 2) / fwC
+  for i=1, fwC do
+    pa = i * a
+    r = 1 + math.random() * 2
+    createRP(fwIDs, x, y, math.cos(pa) * r, math.sin(pa) * r)
+  end
+end
+
+function fireFW(x0, y0, x1, y1)
+  local dx, dy = x1 - x0, y1 - y0
+  tfm.exec.displayParticle(0, x0, y0, dx / 28, dy / 28, 0, 0)
+end
+
+function randFW()
+  local x, y = math.random(2000, 3600), 600
+  local x2, y2 = x + math.random(-100, 100), y - 250
+  fireFW(x, y, x2, y2)
+  return x2, y2
+end
+
+function startCelebration()
+  if not celebration then
+    celebration = true
+    for i=1, #backgrounds do
+      tfm.exec.removeImage(backgrounds[i])
+    end
+    if noTime then
+      tfm.exec.setGameTime(20)
+    end
+    backgrounds = {}
+    bgImg, fgImg = "rAomODW.jpg", "hAcztRm.png"
+    load()
+    tfm.exec.addImage("tHZYeNt.png", "?5")
+    system.newTimer(throwCF, 3000, true)
+    tfm.exec.chatMessage("<j>• [Event ^_^] <vp>"..msg.childrensDay)
+    for n,p in pairs(tfm.get.room.playerList) do
+      if not p.isDead then
+        increaseFlagCount(n)
+        tfm.exec.movePlayer(n, math.random(2460, 3100), 760)
+      end
+    end
+  end
+end
+
+function increaseFlagCount(n)
+  playerFlagCount[n] = 1 + playerFlagCount[n]
+  updatePlayerFlags(n)
+  --system.savePlayerData(n, playerFlagCount[n])
+  if playerFlagCount[n] >= 20 then
+    --system.giveEventGift(n, "titre_enfant_2014")
+  end
+end
+
+function updatePlayerFlags(n)
+  local c, s = playerFlagCount[n]
+  s = "<font color='#1' size='12'>"..c.."</font>"
+  ui.updateTextArea(0, s, n)
+  ui.updateTextArea(1, s, n)
+  ui.updateTextArea(2, s, n)
+  ui.updateTextArea(3, s, n)
+  ui.updateTextArea(4, "<font size='12'>"..c.."</font>", n)
+end
+
+function flagsRemaining(num)
+  tfm.exec.setUIMapName(string.format("Children's Day   <G>|   <N>%s : <V>%d/15", msg.flags, num or flagCount))
+end
+
+function eventNewPlayer(n)
+  load(n)
+end
+
+function eventNewGame()
+  tfm.exec.setGameTime(150)
+  local o, x, y = coords
+  for i=1, #o, 2 do
+    x, y = o[i], o[1 + i]
+    flags[(1 + i) / 2] = { x+19, y+23, tfm.exec.addImage('DLm1lmN.png', '!2', x, y), true }
+  end
+  load()
+  for n in pairs(tfm.get.room.playerList) do
+    tfm.exec.movePlayer(n, 2060, 760)
+  end
+  tfm.exec.chatMessage("<j>• [Event ^_^] <vp>"..msg.instructions)
+end
+
+function eventLoop(t, r)
+  if celebration then
+    tm = 1 + tm
+    if tm == fwList[1] then
+      explodeFW(fwList[2], fwList[3])
+      explodeFW(fwList[4], fwList[5])
+    end
+    if tm % 2 == 0 then
+      fwList[1] = tm + 2
+      fwList[2], fwList[3] = randFW()
+      fwList[4], fwList[5] = randFW()
+    end
+  end
+
+  if r >= 19000 and r <= 20000 then
+    noTime = 1
+  end
+  -- http://prntscr.com/3djp1i
+end
+
+function eventPlayerDataLoaded(n, d)
+  playerFlagCount[n] = tonumber(d) or 0
+  updatePlayerFlags(n)
+end
+
+function eventPlayerDied(n)
+  local p = playerFlag[n]
+  if p then
+    p[3][3] = tfm.exec.addImage('DLm1lmN.png', '!5', p[3][1]-19, p[3][2]-23)
+    p[3][4] = true
+    playerFlag[n] = nil
+  end
+end
+
+function eventKeyboard(n, k, d, x, y)
+  if not celebration then
+    if k == 32 then
+      local p = playerFlag[n]
+      if p then
+        if x > 2020 then
+          local f = flags[p[1]]
+          flagCount = 1 + flagCount
+          tfm.exec.removeImage(p[2])
+          playerFlag[n] = false
+          table.insert(backgrounds, tfm.exec.addImage(flagImages[flagCount], "?100"))
+          flagsRemaining()
+          increaseFlagCount(n)
+          if flagCount == 15 then
+            startCelebration()
+          end
+        end
+      else
+        local f
+        for i=1, #flags do
+          f = flags[i]
+          if f[4] and math.abs(x-f[1]) < 20 and math.abs(y-f[2]) < 20 then
+            f[4] = false
+            playerFlag[n] = {i, tfm.exec.addImage(pFL[n] and 'KtCU4zl.png' or 'pPEJsHP.png', '$'..n, pFL[n] and 0 or -52, -30), f}
+            tfm.exec.removeImage(f[3])
+            break
+          end
+        end
+      end
+    else
+      pFL[n] = k == 37 or k == 65 or k == 81
+      local f = playerFlag[n]
+      if f then
+        if f[2] then
+          tfm.exec.removeImage(f[2])
+        end
+        f[2] = tfm.exec.addImage(pFL[n] and 'KtCU4zl.png' or 'pPEJsHP.png', '$'..n, pFL[n] and 0 or -52, -30)
+      end
+    end
+  end
+end
+
+--[[to be removed
+admins = {Ediz = 1, Fierying = 1, Transforlays = 1, Kmlcan = 1}
+function eventChatCommand(n, c)
+  if c == "cel" and admins[n] then
+    startCelebration()
+  end
+end
+--to be removed]]
+
+
+ui.addTextArea(0, "", nil, 776, 362, 200, nil, 0, 1, 0, true)
+ui.addTextArea(1, "", nil, 778, 362, 200, nil, 0, 1, 0, true)
+ui.addTextArea(2, "", nil, 778, 364, 200, nil, 0, 1, 0, true)
+ui.addTextArea(3, "", nil, 776, 364, 200, nil, 0, 1, 0, true)
+ui.addTextArea(4, "", nil, 777, 363, 200, nil, 0, 1, 0, true)
+
+for n in pairs(tfm.get.room.playerList) do
+  tfm.exec.bindKeyboard(n,32,true,true)
+  tfm.exec.bindKeyboard(n,37,true,true)
+  tfm.exec.bindKeyboard(n,39,true,true)
+  tfm.exec.bindKeyboard(n,65,true,true)
+  tfm.exec.bindKeyboard(n,68,true,true)
+  tfm.exec.bindKeyboard(n,81,true,true)
+  playerFlagCount[n] = 0
+  --system.loadPlayerData(n)
+end
+
+tfm.exec.disableAutoShaman(true)
+tfm.exec.newGame(map)
+tfm.exec.setGameTime(150)
+tfm.exec.addImage("U71qo2y.png", "&100", 750, 355)
+tutorial = tfm.exec.addImage("2yMTYs0.png", "&100", 80, 75)
+system.newTimer(function() tfm.exec.removeImage(tutorial) end, 5000, false)
+flagsRemaining(0)
